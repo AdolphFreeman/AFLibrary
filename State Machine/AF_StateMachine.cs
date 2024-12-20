0036c7cba8861526b971f3d22628091cb0aa85ae
Adolf Freemna <adolphfreeman.xia@gmail.com>
2024-12-20 17:43:29 +0800

    2024.12.20 0

---
+++ State Machine/AF_StateMachine.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AF_StateMachine : MonoBehaviour
+{
+    protected AF_State CurrentState;
+    public string currentStateName;
+
+    private void Update()
+    {
+        CurrentState.StateUpdate();
+    }
+
+    public void ChangeState(AF_State state)
+    {
+        CurrentState?.StateExit();
+        CurrentState = state;
+        CurrentState?.StateEnter();
+        currentStateName = state.ToString();
+    }
+}
+
+public interface AF_State
+{
+    public void StateEnter(){}
+    public void StateUpdate(){}
+    public void StateExit(){}
+}
